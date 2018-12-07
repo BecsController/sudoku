f68c30d8f4826102aeac464e93a0bf1c40e4fdce
becs <becs@storypark.com>
2018-12-07 16:27:49 +1300

    Makes a row of randomly generated 0 to 9

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,14 +1,26 @@
 
 class LandingController < ApplicationController
-  attr_accessor :board
+  attr_accessor :board, :row, :column
+
+  NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   def index
-    @board = default_grid
+    @board = default_board
+    @row = make_row
   end
 
   private
 
-  def default_grid
-    Array.new(9) { Array.new(9) { "X" } }
+  def make_row
+    row = []
+    until row.size == 9 do
+      random = NUMBERS.sample
+      row << random unless row.include? random
+    end
+    row
+  end
+
+  def default_board
+    Array.new(9) { Array.new(9) { "I WANT YOU TO BE A CELL" } }
   end
 end

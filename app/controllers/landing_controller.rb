cf706a95903399ea643027c1da09e1d9dd50f14e
becs <becs@storypark.com>
2018-12-07 15:45:00 +1300

    A whole bunch o x's

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,20 +1,14 @@
+
 class LandingController < ApplicationController
-  attr_accessor :grid
+  attr_accessor :board
 
-  def index(size = {})
-    @board = size.fetch(:grid, default_grid)
-  end
-
-  class Cell
-    attr_accessor :value
-    def initialize(value = "X")
-      @value = value
-    end
+  def index
+    @board = default_grid
   end
 
   private
 
   def default_grid
-    Array.new(9) { Array.new(9) { Cell.new } }
+    Array.new(9) { Array.new(9) { "X" } }
   end
 end
